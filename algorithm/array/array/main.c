6d2b773eae76be706dabdd0e9411ac67ba634383
Seeulater <840787626@qq.com>
2020-11-25 01:19:24 +0800

    初始化

---
+++ algorithm/array/array/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  array
+//
+//  Created by 王燊 on 2020/11/24.
+//  Copyright © 2020 王燊. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ algorithmswift/array/array/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  array
+//
+//  Created by 王燊 on 2020/11/24.
+//  Copyright © 2020 王燊. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
